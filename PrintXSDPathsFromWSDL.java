2f279bfe4ab8193f8f7bfbb0e7d41b2f542b8c40
Chaitanya502 <chaitureddy779@gmail.com>
2024-07-12 15:41:33 +0530

    Update PrintXSDPathsFromWSDL.java

--- PrintXSDPathsFromWSDL.java
+++ PrintXSDPathsFromWSDL.java
@@ -1,20 +1,21 @@
-import org.apache.cxf.wsdl11.WSDLManager;
-import org.apache.cxf.wsdl11.WSDLManagerImpl;
-import org.apache.cxf.wsdl11.WSDLServiceFactory;
-import org.apache.cxf.service.model.ServiceInfo;
-import org.apache.cxf.service.model.SchemaInfo;
+import javax.wsdl.Definition;
+import javax.wsdl.WSDLException;
+import javax.wsdl.factory.WSDLFactory;
+import javax.wsdl.xml.WSDLReader;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
-import java.net.URI;
+import java.io.IOException;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
-/**
-  implementation 'org.apache.cxf:cxf-rt-core:4.0.3'
-    implementation 'org.apache.cxf:cxf-rt-frontend-jaxws:4.0.3'
-    implementation 'org.apache.cxf:cxf-rt-bindings-xml:4.0.3'
-    implementation 'org.apache.cxf:cxf-rt-wsdl:4.0.3'
-    implementation 'org.apache.cxf:cxf-rt-databinding-jaxb:4.0.3'
-**/
+
 public class PrintXSDPathsFromWSDL {
 
     public static void main(String[] args) {
@@ -22,34 +23,92 @@ public class PrintXSDPathsFromWSDL {
             // Path to the WSDL file
             String wsdlPath = "src/main/resources/your.wsdl";
             File wsdlFile = new File(wsdlPath);
-            URI wsdlURI = wsdlFile.toURI();
 
-            // Create WSDL manager
-            WSDLManager wsdlManager = new WSDLManagerImpl();
-            WSDLServiceFactory factory = new WSDLServiceFactory(wsdlManager, wsdlURI.toString());
-            ServiceInfo serviceInfo = factory.create();
+            // Create WSDL reader
+            WSDLFactory factory = WSDLFactory.newInstance();
+            WSDLReader reader = factory.newWSDLReader();
+            Definition definition = reader.readWSDL(wsdlFile.getAbsolutePath());
 
-            // Set to track processed schemas
-            Set<String> processedSchemas = new HashSet<>();
-
-            // Process each schema in the service
-            for (SchemaInfo schemaInfo : serviceInfo.getSchemas()) {
-                processSchema(schemaInfo, processedSchemas);
+            // Get types from the WSDL
+            Map<?, ?> types = definition.getTypes().getExtensibilityElements();
+            Set<String> referencedSchemas = new HashSet<>();
+            for (Object type : types.values()) {
+                if (type instanceof javax.wsdl.extensions.schema.Schema) {
+                    javax.wsdl.extensions.schema.Schema schema = (javax.wsdl.extensions.schema.Schema) type;
+                    Element schemaElement = schema.getElement();
+                    processSchemaElement(schemaElement, referencedSchemas, wsdlFile.getParentFile());
+                }
             }
-        } catch (Exception e) {
+
+            // Remove leftover XSD files
+            removeLeftoverXSDs(wsdlFile.getParentFile(), referencedSchemas);
+
+        } catch (WSDLException | ParserConfigurationException | IOException | SAXException e) {
             e.printStackTrace();
         }
     }
 
-    private static void processSchema(SchemaInfo schemaInfo, Set<String> processedSchemas) {
-        String schemaLocation = schemaInfo.getSystemId();
-        if (schemaLocation != null && !processedSchemas.contains(schemaLocation)) {
-            processedSchemas.add(schemaLocation);
-            System.out.println(schemaLocation);
+    private static void processSchemaElement(Element schemaElement, Set<String> referencedSchemas, File baseDir)
+            throws ParserConfigurationException, IOException, SAXException {
+        // Add the schema element's base URI to the set of referenced schemas
+        String schemaBaseURI = schemaElement.getBaseURI();
+        if (schemaBaseURI != null) {
+            referencedSchemas.add(schemaBaseURI);
+        }
 
-            // Recursively process imported and included schemas
-            for (SchemaInfo importedSchema : schemaInfo.getImportedSchemas()) {
-                processSchema(importedSchema, processedSchemas);
+        // Process <import> elements
+        NodeList importElements = schemaElement.getElementsByTagNameNS("http://www.w3.org/2001/XMLSchema", "import");
+        for (int i = 0; i < importElements.getLength(); i++) {
+            Element importElement = (Element) importElements.item(i);
+            String schemaLocation = importElement.getAttribute("schemaLocation");
+            if (schemaLocation != null && !schemaLocation.isEmpty() && !referencedSchemas.contains(schemaLocation)) {
+                referencedSchemas.add(schemaLocation);
+                File importedSchemaFile = new File(baseDir, schemaLocation);
+                processSchemaFile(importedSchemaFile, referencedSchemas);
+            }
+        }
+
+        // Process <include> elements
+        NodeList includeElements = schemaElement.getElementsByTagNameNS("http://www.w3.org/2001/XMLSchema", "include");
+        for (int i = 0; i < includeElements.getLength(); i++) {
+            Element includeElement = (Element) includeElements.item(i);
+            String schemaLocation = includeElement.getAttribute("schemaLocation");
+            if (schemaLocation != null && !schemaLocation.isEmpty() && !referencedSchemas.contains(schemaLocation)) {
+                referencedSchemas.add(schemaLocation);
+                File includedSchemaFile = new File(baseDir, schemaLocation);
+                processSchemaFile(includedSchemaFile, referencedSchemas);
+            }
+        }
+    }
+
+    private static void processSchemaFile(File schemaFile, Set<String> referencedSchemas)
+            throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        InputSource inputSource = new InputSource(schemaFile.getAbsolutePath());
+        Element schemaElement = builder.parse(inputSource).getDocumentElement();
+        processSchemaElement(schemaElement, referencedSchemas, schemaFile.getParentFile());
+    }
+
+    private static void removeLeftoverXSDs(File baseDir, Set<String> referencedSchemas) {
+        // List all XSD files in the base directory
+        File[] xsdFiles = baseDir.listFiles((dir, name) -> name.endsWith(".xsd"));
+        if (xsdFiles != null) {
+            for (File xsdFile : xsdFiles) {
+                try {
+                    String canonicalPath = xsdFile.getCanonicalPath();
+                    // If the XSD file is not in the set of referenced schemas, delete it
+                    if (!referencedSchemas.contains(canonicalPath)) {
+                        if (xsdFile.delete()) {
+                            System.out.println("Deleted unused XSD file: " + canonicalPath);
+                        } else {
+                            System.out.println("Failed to delete unused XSD file: " + canonicalPath);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
